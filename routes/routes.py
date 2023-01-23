1257fe042d7407461493d509f7388b6169e04552
Gabriel Gomes da Silva <72311029+Bielgomes@users.noreply.github.com>
2023-01-23 17:01:52 -0300

    fix: Routes

--- routes/routes.py
+++ routes/routes.py
@@ -8,11 +8,7 @@ last_request_time: Dict[str, float] = {}
 rate_limit = 300
 current_time = time.time()
 
-routes = Routes(static_path='static')
-
-@routes.get('/post')
-async def index(req: Request, res: Response):
-  res.render_page('index.html').status(200).send()
+routes = Routes(static_path='static').cors()
 
 @routes.post('/')
 async def main(req: Request, res: Response):
@@ -39,15 +35,4 @@ async def main(req: Request, res: Response):
 @routes.get('/')  
 async def main(req: Request, res: Response):
   response = Service().get_random()
-  html = f"""
-  <head> 
-    <meta charset="UTF-8">
-  </head>
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
-  <body> 
-    <h1> {response['author']} uma vez disse:</h1>
-    <h2> {response['message']} </h2>
-    <h3> {response['date']} </h3>
-  </body>
-  """
-  res.html(html).status(200).send()
+  res.json(response).status(200).send()
